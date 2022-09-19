8b2564601517a49f280ea1911c5382bafa51ff47
Alba <113705846+Meencanta2022@users.noreply.github.com>
2022-09-19 11:10:56 -0400

    Update streamlit_app.py
    Added a new header after listing

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,10 @@ streamlit.title("My Parents New Healthy Diner")
 
 streamlit.header("Breakfast Menu")
 
-streamlit.text("🥣 Omega 3 & BlueBerry Oatmeal")
-streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
-streamlit.text("🐔 Hard-Boiled Free-Range Egg")
-streamlit.text("🥑🍞 Avocado Toast")
+streamlit.text('🥣 Omega 3 & BlueBerry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
