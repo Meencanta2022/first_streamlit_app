819e1a79ce632b71a82eab3d4027e33dba2a8894
Alba <113705846+Meencanta2022@users.noreply.github.com>
2022-09-23 11:26:06 -0400

    Update streamlit_app.py
    Added STOP command to avoid running snowflake and move the import command to the top of the page. Also added an Error import

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
-import streamlit  
+import streamlit 
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title("My Parents New Healthy Diner")
 
@@ -12,7 +16,7 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
+#import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 #We want the customer to be able to choose the fruits by name!!
@@ -30,7 +34,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-import requests
+#import requests
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
@@ -39,6 +43,9 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
+
+#STOP Command to Focus Our Attention
+streamlit.stop()
 
 import snowflake.connector
 
@@ -59,4 +66,8 @@ streamlit.write('Thanks for adding ', add_my_fruit)
 
 # this will not work correctly
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+
+
+
+
 
